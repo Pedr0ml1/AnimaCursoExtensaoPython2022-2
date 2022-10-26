f4bdc96e96c08e3c69438e4b057d6ff22ba3f615
Pedr0ml1 <116230310+Pedr0ml1@users.noreply.github.com>
2022-10-26 22:46:36 +0000

    adicionei os inputs

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ nome = "Pedro Munhoz"
 idade = 21
 print("Meu nome é "+nome)
 print("minha idade é "+str(idade)+" anos")
+nome = input("Qual é o seu nome? ")
+input(f"Boa noite, {nome}")
+idade = input("Quantos anos você tem? ")
+input(f"sua idade é {idade}")
